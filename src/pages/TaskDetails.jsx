4b04d89001f03e4aeabe49fb27785e9ce68ad30b
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2024-12-12 10:35:02 -0500

    finished team and trash page components

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -1,4 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clsx from "clsx"; 
+import moment from "moment"; 
+import { FaBug, FaTasks, FaThumbsUp, FaUser } from "react-icons/fa"; 
+import { GrInProgress } from "react-icons/gr"; 
+import {
+  MdKeyboardArrowDown, 
+  MdKeyboardArrowUp, 
+  MdKeyboardDoubleArrowUp, 
+  MdOutlineDoneAll,
+  MdOutlineMessage, 
+  MdTaskAlt
+} from "react-icons/md"; 
+import { RxActivityLog } from "react-icons/rx"; 
+import { useParams } from "react-router-dom"; 
+import { toast } from "sonner"; 
+
+const assets = [
+  "https://images.pexels.com/photos/2418664/pexels-photo-2418664.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/8797307/pexels-photo-8797307.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/2534523/pexels-photo-2534523.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/804049/pexels-photo-804049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+];
+
+const ICONS = {
+  high: <MdKeyboardDoubleArrowUp />,
+  medium: <MdKeyboardArrowUp />,
+  low: <MdKeyboardArrowDown />,
+};
+
+const bgColor = {
+  high: "bg-red-200",
+  medium: "bg-yellow-200",
+  low: "bg-blue-200",
+};
+
+const TABS = [
+  { title: "Task Detail", icon: <FaTasks /> }, 
+  { title: "Activities/Timeline", icon: <RxActivityLog /> }
+];
+
+
+
 
 const TaskDetails = () => {
   return (
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,14 @@
+export const formatDate = (date) => {
+  // Get the month, day, and year
+  const month = date.toLocaleString("en-US", { month: "short" });
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  const formattedDate = `${day}-${month}-${year}`;
+
+  return formattedDate;
+};
+
 export function getInitials(fullName) {
   const names = fullName.split(" "); 
 
